8603bfbe68acb4e129b67538828f53bf207b06d8
Jesvin Sebastian Madona <sebastianjesvin@gmail.com>
2021-09-24 16:51:15 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="container">
+			<div className="row ml-5">
+					<div className="col-sm-6">
+						<p>testing</p>
+					</div>
+					<div className="col-sm-6">
+						<p>boostrap</p>
+					</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
